da386f5dd5e0b9428619df203d8e4c58a158aa74
Vu Minh Le <vuminhle@outlook.com>
2025-06-04 23:40:49 +0200

    Refactor create_agent function to remove memory parameter and update docstring for clarity

--- backend/services/llm_agent.py
+++ backend/services/llm_agent.py
@@ -4,44 +4,39 @@ import uuid
 
 from datetime import datetime
 from llama_index.core import PromptTemplate
-from llama_index.core.agent import ReActAgent
-from llama_index.core.base.llms.types import MessageRole, ChatMessage
-from llama_index.core.chat_engine.types import StreamingAgentChatResponse
-from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.core.agent.workflow import ReActAgent
 from typing import List, AsyncGenerator
 from llama_index.core.tools import BaseTool
+from llama_index.llms.ollama import Ollama
 from sqlmodel import Session
 from models import Chat
 
 from dependencies import logger
 
-def create_agent(memory: ChatMemoryBuffer, system_prompt: PromptTemplate, tools: List[BaseTool],
+def create_agent(system_prompt: str, tools: List[BaseTool],
+                 llm: Ollama,
                  **kwargs) -> ReActAgent:
     """
     Creates and configures a ReActAgent with specified parameters.
 
     Args:
-        memory (ChatMemoryBuffer): Memory buffer to store conversation history
-        system_prompt (PromptTemplate): System prompt template for the agent
+        system_prompt (str): System prompt template for the agent
         tools (List[BaseTool]): List of tools available to the agent
-        **kwargs: Additional keyword arguments to pass to ReActAgent.from_llm()
+        llm (Ollama): Ollama
+        **kwargs: Additional keyword arguments to pass to ReActAgent
 
     Returns:
         ReActAgent: Configured agent instance with updated prompts and specified parameters
 
     Example:
-        >>> memory = ChatMemoryBuffer()
-        >>> system_prompt = PromptTemplate("You are a helpful assistant")
+        >>> system_prompt = "You are a helpful assistant"
         >>> tools = [Tool1(), Tool2()]
-        >>> agent = create_agent(memory, system_prompt, tools)
+        >>> agent = create_agent(system_prompt, tools, llm)
     """
-    agent = ReActAgent.from_llm(
-        system_prompt=system_prompt,
-        memory=memory,
-        max_iterations=50,
-        **kwargs,
-        verbose=True,
+    agent = ReActAgent(
+        llm=llm,
         tools=tools,
+        system_prompt=system_prompt,
+        **kwargs,
     )
-    agent.update_prompts({"agent_worker:system_prompt": system_prompt})
     return agent
